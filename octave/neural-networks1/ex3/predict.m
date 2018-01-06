68b7e0699810ca8da555bef71fe2e53d8f05344a
Neil Alishev <alishev.neil@gmail.com>
2018-01-06 19:15:21 +0300

    implement neural network forward propagation

--- octave/neural-networks1/ex3/predict.m
+++ octave/neural-networks1/ex3/predict.m
@@ -7,12 +7,14 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+X = [ones(m, 1) X];
+
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -21,12 +23,16 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+for i = 1:m
+    % first step of forward propagation (first hidden layer)
+    first_layer_answers = sigmoid(X(i, :) * Theta1');
+    first_layer_answers = [1 first_layer_answers]; % add bias unit
 
-
-
-
-
-
+    % second step of forward propagation (output layer)
+    second_layer_answers = sigmoid(first_layer_answers * Theta2');
+    [_, idx] = max(second_layer_answers);
+    p(i, :) = idx;
+end
 
 
 % =========================================================================
